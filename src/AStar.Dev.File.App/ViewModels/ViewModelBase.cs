a863bf9b88daba8cdf34fa07dae103d7191c4fb4
Jason Barden <jason.barden1@outlook.com>
2026-03-11 01:37:26 +0000

    feat: add scaffolded projects

---
+++ src/AStar.Dev.File.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AStar.Dev.File.App.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ src/AStar.Dev.File.App/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AStar.Dev.File.App.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
